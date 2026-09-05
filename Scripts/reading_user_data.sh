a7c5167e06d9124f81c2e2af3c8460478990a603
nsaikumar <nagaroorusaikumar@gmail.com>
2026-09-05 12:02:07 +0530

    reading Users data

---
+++ Scripts/reading_user_data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Enter your name:"
+read name
+
+echo "Hi $name, this is bash scripting practice."
+
+read -p 'Username: ' USR
+read -sp 'Password: ' pass
+
+echo
+
+echo "Login Successfull : Welcome $USR."
