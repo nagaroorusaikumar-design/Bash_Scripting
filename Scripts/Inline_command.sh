3a8f4a6f59dd440542a176c4f35ee77d6ad1cc0d
nsaikumar <nagaroorusaikumar@gmail.com>
2026-09-05 10:51:01 +0530

    Inline command

---
+++ Scripts/Inline_command.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Location of the File"
+echo $0
+echo
+
+echo "My Name is "
+echo $1
+
+echo
+
+echo "My heighes graduation is "
+echo $2
+
+echo
+echo "My present age is "
+echo $3
