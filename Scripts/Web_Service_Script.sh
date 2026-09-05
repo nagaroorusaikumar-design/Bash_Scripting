58ab82bbb8c869460acaabe035e88156ecadcd68
nsaikumar <nagaroorusaikumar@gmail.com>
2026-09-05 08:54:09 +0530

    Web Service Setup Bash Script

---
+++ Scripts/Web_Service_Script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#Installing Dependencies
+sudo yum install wget unzip httpd -y
+
+#Start and Enable service
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+#Creating temporary directory
+mkdir -p /tmp/webfiles
+cd /tmp/webfiles
+
+#Getting the template
+wget https://www.tooplate.com/zip-templates/2098_health.zip
+
+#Unzipping
+unzip 2098_health.zip
+
+#Coping the file
+sudo cp -r 2098_health.zip/*/var/www/html/
+
+#Restarting the service
+sudo systemctl restart httpd
+
+#Cleaning the webfiles
+rm -rf /tmp/webfiles
